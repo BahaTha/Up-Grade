123aadab2508c4a9c0a28bd3744f03349fc5968b
BahaGodx <baha.thabet@live.ca>
2022-04-13 00:01:39 +0100

    aa

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Delsoss
+# Up-grade
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.1.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/Delsoss'),
+      dir: require('path').join(__dirname, './coverage/Upgrade'),
       subdir: '.',
       reporters: [
         { type: 'html' },
