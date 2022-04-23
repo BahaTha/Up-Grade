37018eb13cf922210e0893e6e3efb1423686683b
BahaGodx <baha.thabet@live.ca>
2022-04-23 23:05:28 +0100

    Fixes

--- angular-frontend/src/app/login/login.component.ts
+++ angular-frontend/src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { AuthService } from '../auth.service';
+import { AuthService } from '../Services/auth.service';
+
 
 @Component({
   selector: 'app-login',
