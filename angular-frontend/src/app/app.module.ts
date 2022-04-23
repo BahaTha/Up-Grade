eb8d8d24b5c993ea89d4c934a8beb43902b7ca24
ines-kr <ineskrichene87@gmail.com>
2022-04-23 23:51:45 +0100

    work

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -34,7 +34,7 @@ import { RouterModule } from '@angular/router';
 import { AddStudentComponent } from './admin/add-student/add-student.component';
 import { AddInstructorComponent } from './admin/add-instructor/add-instructor.component';
 import { AddCourseComponent } from './admin/add-course/add-course.component';
-import { AuthService } from './Services/auth.service';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
