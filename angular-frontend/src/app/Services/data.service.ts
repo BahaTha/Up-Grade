1fe393e7c5f6d6ddd2735b34244279b2730e9734
ines-kr <ineskrichene87@gmail.com>
2022-04-24 15:24:49 +0100

    add forms work

--- angular-frontend/src/app/Services/data.service.ts
+++ angular-frontend/src/app/Services/data.service.ts
@@ -1,27 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-const baseUrl = 'http://localhost:8081/category';
+const baseUrl = 'http://localhost:8081';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CategoryService {
+export class DataService {
   constructor(private http: HttpClient) { }
   getAll(): Observable<any> {
-    return this.http.get(baseUrl+'/all');
+    return this.http.get(baseUrl);
   }
   get(id:any ): Observable<any> {
     return this.http.get(`${baseUrl}/${id}`);
   }
   create(data:any): Observable<any> {
-    return this.http.post(baseUrl+'/add', data);
+    return this.http.post(baseUrl, data);
   }
   update(id:any, data:any): Observable<any> {
     return this.http.put(`${baseUrl}/${id}`, data);
   }
   delete(id:any): Observable<any> {
-    return this.http.delete(`${baseUrl+'/delete'}/${id}`);
+    return this.http.delete(`${baseUrl}/${id}`);
   }
   deleteAll(): Observable<any> {
     return this.http.delete(baseUrl);
