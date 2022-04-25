9a2e25e30b914f5789dd8fbc0e0b74f142756c38
ines-kr <ineskrichene87@gmail.com>
2022-04-25 04:00:13 +0100

    a

--- spring-backend/src/main/java/com/example/demo/controller/UserController.java
+++ spring-backend/src/main/java/com/example/demo/controller/UserController.java
@@ -65,7 +65,6 @@ public class UserController {
          }catch (Exception e) {
         	 return new ResponseEntity <>(HttpStatus.INTERNAL_SERVER_ERROR);
          }
-        	 
     }
 	
 	@GetMapping("/role")
