5a927abaddcf2c1d0abefcc70617e126ff89a261
BahaGodx <baha.thabet@live.ca>
2022-04-25 01:21:03 +0100

    a

---
+++ angular-frontend/src/app/admin/admin/admin.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ViewChild, AfterViewInit } from '@angular/core';
+import { SidebarAdminComponent } from './sidebar-admin/sidebar-admin.component';
+
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.component.html',
+  styleUrls: ['./admin.component.css']
+})
+export class AdminComponent implements OnInit {
+  constructor() { }
+  ngOnInit() { }
+}
